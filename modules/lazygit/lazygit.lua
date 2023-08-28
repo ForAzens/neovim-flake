81b6710ec098b88b6296935eb308d702876d300d
Daniel C <azenis.ofc@gmail.com>
2023-08-28 18:44:51 +0200

    Add more telescope keymaps

--- modules/lazygit/lazygit.lua
+++ modules/lazygit/lazygit.lua
@@ -1,7 +1,3 @@
 local opt = { noremap = true, silent = true }
 
 vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>:LazyGit<CR>", opt)
-vim.api.nvim_set_keymap("n", "<leader>gc", ":lua require('telescope.builtin').git_commits()<CR>", opt)
-vim.api.nvim_set_keymap("n", "<leader>gs", ":lua require('telescope.builtin').git_status()<CR>", opt)
-vim.api.nvim_set_keymap("n", "<leader>gb", ":lua require('telescope.builtin').git_branches()<CR>", opt)
-vim.api.nvim_set_keymap("n", "<leader>gb", ":lua require('telescope.builtin').git_branches()<CR>", opt)
--- modules/telescope/telescope.lua
+++ modules/telescope/telescope.lua
@@ -2,5 +2,14 @@ local opt = { noremap = true, silent = true }
 local telescope = require("telescope")
 
 telescope.setup({})
--- vim.api.nvim_set_keymap("n", "<leader><leader>", ":lua require('telescope.builtin').find_files()<CR>", opt)
-vim.keymap.set("n", "<leader><leader>", ":lua require('telescope.builtin').find_files()<CR>", opt)
+vim.keymap.set("n", "<leader><leader>", ":lua require('telescope.builtin').find_files()<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>/", ":lua require('telescope.builtin').live_grep()<CR>", { desc = "Grep (root dir)" })
+vim.keymap.set("n", "<leader>:", ":Telescope command_history<CR>", { desc = "Command history"} )
+vim.keymap.set("n", "<leader>,", ":Telescope buffers show_all_buffers=true<CR>", { desc = "Switch Buffer" })
+
+vim.keymap.set("n", "<leader>gc", ":lua require('telescope.builtin').git_commits()<CR>", { desc = "commits" })
+vim.keymap.set("n", "<leader>gs", ":lua require('telescope.builtin').git_status()<CR>", { desc = "status" })
+vim.keymap.set("n", "<leader>gb", ":lua require('telescope.builtin').git_branches()<CR>", {desc = "branches" })
+
+vim.keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", {desc = "Recent"})
+vim.keymap.set("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<CR>", { desc = "Find files" })
