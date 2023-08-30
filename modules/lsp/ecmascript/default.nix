82274eb248adb8be04ad69b8fb49706201603236
Daniel C <azenis.ofc@gmail.com>
2023-08-30 22:05:32 +0200

    Disable tsserver formatting

--- modules/lsp/ecmascript/default.nix
+++ modules/lsp/ecmascript/default.nix
@@ -8,6 +8,7 @@ let
   content = ''
     local nvim_lsp = require("lspconfig")
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    capabilities.documentFormattingProvider = false
 
     nvim_lsp.tsserver.setup({
       capabilities = capabilities,
