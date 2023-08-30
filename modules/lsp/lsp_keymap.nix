d75ccd600943a9fa4282a3b4e74018b699c3bddc
Daniel C <azenis.ofc@gmail.com>
2023-08-30 21:44:55 +0200

    Add LSP keybindings

--- modules/lsp/lsp_keymap.nix
+++ modules/lsp/lsp_keymap.nix
@@ -1,10 +1,42 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, ... }:
 
 
 let
   content = ''
-vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format current buffer" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+
+		local local_map = function(mode, lhs, rhs, opts)
+      local shared_opts = { buffer = ev.buf }
+			vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", shared_opts, opts or {}))
+		end
+
+		local_map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+		local_map("n", "<leader>cl", "<cmd>LspInfo<cr>", { desc = "LspInfo" })
+		local_map("n", "gd", function()
+			require("telescope.builtin").lsp_definitions({ reuse_win = true })
+		end, { desc = "Goto Definition" })
+		local_map("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+		local_map("n", "gr", "<cmd>Telescope lsp_references<cr>", { desc = "References" })
+		local_map("n", "gI", function()
+			require("telescope.builtin").lsp_implementations({ reuse_win = true })
+		end, { desc = "Goto Implementation" })
+		local_map("n", "gy", function()
+			require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
+		end, { desc = "Goto T[y]pe Definition" })
+		local_map("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+		local_map("n", "gK", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+		local_map("i", "<c-k>", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+		local_map("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format Document" })
+		local_map("v", "<leader>cf", vim.lsp.buf.format, { desc = "Format Range" })
+		local_map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+		local_map("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+		local_map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
+	end,
+})
   '';
   luaFile = pkgs.writeText "lsp_keymap.lua" content;
 in
