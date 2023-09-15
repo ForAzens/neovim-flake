650e28fbdc5a8e9741a148e84cfc6dab32e29a11
Daniel C <azenis.ofc@gmail.com>
2023-09-15 20:50:16 +0200

    Try prettier instead of prettierd

--- modules/lsp/null_ls.nix
+++ modules/lsp/null_ls.nix
@@ -15,7 +15,7 @@ let
             null_ls.builtins.code_actions.eslint_d,
             null_ls.builtins.diagnostics.eslint_d,
             null_ls.builtins.formatting.eslint_d,
-            null_ls.builtins.formatting.prettierd,
+            null_ls.builtins.formatting.prettier,
           ''}
           ${writeIf cfg.ruby.enable ''null_ls.builtins.formatting.rubocop.with({
               command = "bundle",
