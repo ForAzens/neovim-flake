667b92aeb72b007951d857a0bd97fcc570f9351c
Daniel Castillo <daniel@Kelsier.local>
2024-01-07 17:58:12 +0100

    Add prettier support for elixir and heex

--- modules/lsp/null_ls.nix
+++ modules/lsp/null_ls.nix
@@ -15,7 +15,9 @@ let
             null_ls.builtins.code_actions.eslint_d,
             null_ls.builtins.diagnostics.eslint_d,
             null_ls.builtins.formatting.eslint_d,
-            null_ls.builtins.formatting.prettier,
+            null_ls.builtins.formatting.prettier.with({
+                extra_filetypes = { "elixir", "heex" }
+              }),
           ''}
         },
     })
